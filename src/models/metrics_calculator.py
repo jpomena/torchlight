5566aabb1a708366330ee068b0424404151949ac
jpomena <220436756+jpomena@users.noreply.github.com>
2025-08-27 16:56:00 -0300

    Initial commit

--- src/models/metrics_calculator.py
+++ src/models/metrics_calculator.py
@@ -1,4 +1,4 @@
-import statistics
+import statistics as st
 
 
 class MetricsCalculator:
@@ -17,4 +17,21 @@ class MetricsCalculator:
         return task_metrics
 
     def get_tasks_statics(tasks_rt, tasks_ct, tasks_lt):
-        
+        rt_average = st.mean(tasks_rt)
+        rt_population_st_dev = st.pstdev(tasks_rt)
+
+    def linreg(X, Y):
+        N = len(X)
+        Sx = Sy = Sxx = Syy = Sxy = 0.0
+        for x, y in zip(X, Y):
+            Sx = Sx + x
+            Sy = Sy + y
+            Sxx = Sxx + x*x
+            Syy = Syy + y*y
+            Sxy = Sxy + x*y
+        det = Sxx * N - Sx * Sx
+        return (Sxy * N - Sy * Sx)/det, (Sxx * Sy - Sx * Sxy)/det
+
+
+    x = [12, 34, 29, 38, 34, 51, 29, 34, 47, 34, 55, 94, 68, 81]
+    a,b = linreg(range(len(x)),x)  //your x,y are switched from standard notation
